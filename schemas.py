93b7068f393880937180eddb3f691bb0824fdffe
scotttyso <tyscott@cisco.com>
2023-11-29 11:26:37 -0500

    reduced data output from python script

--- schemas.py
+++ schemas.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+from copy import deepcopy
 from dotmap import DotMap
+from operator import itemgetter
 import json
 import re
-jdata = DotMap(json.load(open('schema.json', 'r')))
+import sys
+import yaml
+jdata = DotMap(json.load(open(f'{sys.argv[1]}', 'r')))
 schemas = DotMap()
 print(json.dumps(jdata, indent=4))
 for item in jdata.schemas:
@@ -18,16 +22,21 @@ for item in jdata.schemas:
             for d in e.epgs:
                 if ':' in d.displayName: description = d.displayName.split(':')[1]
                 else: description = d.description
-                print(d.bdRef)
                 bd = re.search('bds\/([\\w\\-\\.]+)$', d.bdRef).group(1)
                 schemas[item.displayName].templates[i.name].epgs.append(DotMap(
                     application_profile = e.name,
-                    description = description,
-                    name = d.name,
-                    vrf = schemas[item.displayName].templates[i.name].bds[bd].vrf.name
+                    description         = d.displayName.replace(f'{d.name}-', ''),
+                    name                = d.name,
                 ))
-print(f'{"=" * 91}\n  AAEP with EPG Mappings\n{"=" * 91}')
-print(json.dumps(schemas, indent=4))
-outfile = open('./schema_out.json', 'w')
-outfile.write(json.dumps(schemas, indent=4))
+schemas.pop('common')
+check = deepcopy(schemas)
+for key, value in check.items():
+    for k, v in value.templates.items():
+        if v.get('bds'): schemas[key].templates[k].pop('bds')
+        schemas[k].templates[k].epgs = sorted(v.epgs, key=itemgetter('name'))
+print(f'{"=" * 91}\n  Display Description Mappings\n{"=" * 91}')
+schema_out = schemas.toDict()
+#print(json.dumps(schemas, indent=4))
+outfile = open('./schema_out.yaml', 'w')
+outfile.write(yaml.dump(schema_out, indent=4))
 outfile.close()
